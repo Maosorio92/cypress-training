69bfbf80b3307ca3557665854ba7ec8445d2bd76
Manuel Alejandro Osorio V <maaosoriova@gmail.com>
2022-07-31 13:16:24 -0500

    POM Rev6

--- cypress/page/login.page.ts
+++ cypress/page/login.page.ts
@@ -1,17 +1,17 @@
 class LoginPage {
-    private Email: string;
-    private Password: string;
+    private email: string;
+    private password: string;
     private submit: string;           
 
     constructor() {
-        this.Email ="#email";
-        this.Password ="#passwd";
+        this.email ="#email";
+        this.password ="#passwd";
         this.submit = "[name=SubmitLogin]";        
     }
     
     public signIn(userEmail :string, userPassword : string): void {
-        cy.get(this.Email).type(userEmail)
-        cy.get(this.Password).type(userPassword)
+        cy.get(this.email).type(userEmail)
+        cy.get(this.password).type(userPassword)
         cy.get(this.submit).click()
     }      
 }
