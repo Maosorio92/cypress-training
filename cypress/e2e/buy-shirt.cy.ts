b878f090ff64095b4b80eef8c159a7a709140e2b
Manuel Alejandro Osorio V <maaosoriova@gmail.com>
2022-08-01 22:25:14 -0500

    AAA

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -8,27 +8,41 @@ import {
   PaymentStepPage,
 } from "../page/index";
 
-const menuContentPage = new MenuContentPage();
-const productsListPage = new ProductslistPage();
-const shopingCartPage = new ShopingCartPage();
-const loginPage = new LoginPage();
-const adressStepPage = new AdressStepPage();
-const shippingStepPage = new ShippingStepPage();
-const paymentStepPage = new PaymentStepPage();
-
 describe("Buy a t-shirt", () => {
+  let menuContentPage : MenuContentPage;
+  let productsListPage : ProductslistPage;
+  let shopingCartPage : ShopingCartPage;
+  let loginPage : LoginPage;
+  let adressStepPage : AdressStepPage;
+  let shippingStepPage : ShippingStepPage;
+  let paymentStepPage : PaymentStepPage;
+
+  before(() => {
+    menuContentPage = new MenuContentPage();
+    productsListPage = new ProductslistPage();
+    shopingCartPage = new ShopingCartPage();
+    loginPage = new LoginPage();
+    adressStepPage = new AdressStepPage();
+    shippingStepPage = new ShippingStepPage();
+    paymentStepPage = new PaymentStepPage();
+  });
+
   it("then should be bought a t-shirt", () => {
+    const userEmail="aperdomobo@gmail.com";
+    const userPassword="WorkshopProtractor";
     menuContentPage.visitMenuContentPage();
+
     menuContentPage.goToTShirtMenu();
     productsListPage.goToProductList();
     productsListPage.proceedToCheckout();
     shopingCartPage.proceedToCheckout();
-    loginPage.signIn("aperdomobo@gmail.com", "WorkshopProtractor");
+    loginPage.signIn(userEmail, userPassword);
     adressStepPage.proceedToCheckout();
     shippingStepPage.acceptTerms();
     shippingStepPage.proceedToCheckout();
     paymentStepPage.goToPayment();
     paymentStepPage.proceedToCheckout();
+
     paymentStepPage.getConfirm().should(
         "have.text",
         "Your order on My Store is complete.",
