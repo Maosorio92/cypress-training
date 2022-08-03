ec83bf0e8f14e5b75ec4760c2fbeb92eebfce2a8
Manuel Alejandro Osorio V <maaosoriova@gmail.com>
2022-08-02 22:26:21 -0500

    Method filter and find

--- cypress/page/products-list.page.ts
+++ cypress/page/products-list.page.ts
@@ -1,18 +1,25 @@
+import { contains } from "cypress/types/jquery";
+
 class ProductslistPage {
     private productList: string;
-    private proceedToCheckoutProduct: string;     
+    private proceedToCheckoutProduct: string;  
+    private shirtsList : string;   
 
     constructor() {
         this.productList ="a[title='Add to cart']";	    
-        this.proceedToCheckoutProduct ="a[title='Proceed to checkout']";	    
+        this.proceedToCheckoutProduct ="a[title='Proceed to checkout']";	
+        this.shirtsList = ".product-container";    
     }
 
     
-    public goToProductList(): void {
-        cy.get(this.productList).click()
+    public addTShirtToCart(name:string): void {
+        this.findProductByName(name).find(this.productList).click()        
     }
     public proceedToCheckout(): void {
         cy.get(this.proceedToCheckoutProduct).click()
+    }
+    public findProductByName(name:string): any {
+        return cy.get(this.shirtsList).filter(`:contains('${name}')`)
     }
 }
 
